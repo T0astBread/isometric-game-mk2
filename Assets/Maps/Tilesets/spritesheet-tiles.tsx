15befd46bccac961847440d7c33e1f1701f15010
T0astBread <tb@t0ast.cc>
2019-01-17 08:57:36 +0100

    Added Tiled map with Tiled2Unity

---
+++ Assets/Maps/Tilesets/spritesheet-tiles.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="spritesheet-tiles" tilewidth="32" tileheight="16" tilecount="6" columns="1">
+ <grid orientation="isometric" width="32" height="16"/>
+ <image source="../../Sprites/Generated/Tiles_spritesheet.png" width="32" height="96"/>
+</tileset>
