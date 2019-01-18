273663a597ccde64b1f858108b40cf56dcf6ce09
T0astBread <t0astbreaaad@gmail.com>
2019-01-18 20:10:33 +0100

    Added changes that I somehow lost
    This includes:
      - Fixing the sprite scaling/compression
      - Having the camera follow the player

---
+++ Assets/Scripts/FollowBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBehaviour : MonoBehaviour
+{
+	public GameObject target;
+	public float maxDistance = 0;
+
+	private MovementControls targetMovementControlBehaviour;
+
+	void Start()
+	{
+		this.targetMovementControlBehaviour = this.target.GetComponent<MovementControls>();
+	}
+
+	void Update()
+	{
+		if(this.targetMovementControlBehaviour == null || this.targetMovementControlBehaviour.canMove)
+		{
+			var distanceVec = (Vector2)(this.target.transform.position - transform.position);
+			var distance = distanceVec.magnitude;
+			var distanceToMove = distance - this.maxDistance;
+
+			if(distanceToMove > .001f)
+			{
+				transform.position += (Vector3)(distanceVec.normalized * distanceToMove);
+			}
+		}
+	}
+}
