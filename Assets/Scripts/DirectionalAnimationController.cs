4c4b5685c2851b8846da0e503a47214cbbff28ff
T0astBread <t0astbreaaad@gmail.com>
2019-01-19 13:13:46 +0100

    Added Slime enemy that doesn't currently do anything

---
+++ Assets/Scripts/DirectionalAnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalAnimationController : MonoBehaviour
+{
+	public RuntimeAnimatorController northEastAC, northWestAC, southEastAC, southWestAC;
+	public float threshold = .01f;
+
+	private Rigidbody2D rigidbody;
+	private Animator animator;
+	private RuntimeAnimatorController[] controllers;
+
+	void Start()
+	{
+		this.rigidbody = GetComponent<Rigidbody2D>();
+		this.animator = GetComponent<Animator>();
+		this.controllers = new RuntimeAnimatorController[] {
+			this.northEastAC,
+			this.northWestAC,
+			this.southEastAC,
+			this.southWestAC
+		};
+	}
+
+	void Update()
+	{
+		if (this.rigidbody.velocity.magnitude > this.threshold)
+		{
+			int directionIndex = 0;
+			directionIndex += this.rigidbody.velocity.x > 0 ? 1 : 0;
+			directionIndex += this.rigidbody.velocity.y > 0 ? 0 : 2;
+			this.animator.runtimeAnimatorController = this.controllers[directionIndex];
+		}
+	}
+}
