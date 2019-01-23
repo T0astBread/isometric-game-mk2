788432b9921baa95231418be4c4f707651713d50
T0astBread <tb@t0ast.cc>
2019-01-23 02:05:05 +0100

    Added damage popup

---
+++ Assets/Scripts/UI/DamagePopup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePopup : MonoBehaviour
+{
+	public int damagePointsToShow = 0;
+
+	void Start()
+	{
+		GetComponentInChildren<TMPro.TextMeshProUGUI>().text = this.damagePointsToShow.ToString();
+	}
+}
