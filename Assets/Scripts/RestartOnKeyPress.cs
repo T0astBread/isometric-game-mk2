260e0786d71f429bc93fbd93b45752cfc0ce5518
T0astBread <tb@t0ast.cc>
2019-01-28 20:09:43 +0100

    Moved score reset to restart instead of death

--- Assets/Scripts/RestartOnKeyPress.cs
+++ Assets/Scripts/RestartOnKeyPress.cs
@@ -6,12 +6,18 @@ using UnityEngine.SceneManagement;
 public class RestartOnKeyPress : MonoBehaviour
 {
 	public string key = "r";
+	public bool resetScore;
 
 	void Update()
 	{
 		if (Input.GetKeyUp(this.key))
 		{
 			SceneManager.LoadScene(gameObject.scene.name, LoadSceneMode.Single);
+
+			if (this.resetScore)
+			{
+				GameObject.FindObjectOfType<ScoreKeeper>().ResetScore();
+			}
 		}
 	}
 }
